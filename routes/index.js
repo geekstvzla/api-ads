205aaa1d87995cf226f49e113b1a197b7364fb7f
David <dmolina101@gmail.com>
2023-08-11 12:59:55 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/login', async function(req, res, next) {
+
+  const options = {
+      method: 'post',
+      data: {'codigo': 'bdso9f', 'correo': 'manuales2010@gmail.com', 'token' : 'QldmZUhaNnkzcGxJbFNiZ0xKenlFQT09'},
+      url: 'http://172.16.40.58/api/v1/GetClientsDetails'
+};
+
+  await axios(options)
+  .then(async function (response) {
+
+      let userData = response.data.datos[0]
+ 
+      console.log(userData)
+  })
+  .catch(function(error) { 
+
+      console.log("ERROR")
+      console.log(error.data)
+
+  })
+
+  res.send('SAPE 7')
+
+})
+
+router.get('/test', async function(req, res, next) {
+
+    const options = {
+        method: 'post',
+        data: {'idcliente': 2, 'token' : 'QldmZUhaNnkzcGxJbFNiZ0xKenlFQT09'},
+        url: 'http://172.16.40.58/api/v1/GetClientsDetails'
+    };
+
+    await axios(options)
+    .then(async function (response) {
+        console.log(response.data)
+    })
+    .catch(error => 
+
+        console.log("ERROR")
+        //console.log(error.data)
+    )
+    
+    res.send('SAPE 2')
+
 });
 
 module.exports = router;
