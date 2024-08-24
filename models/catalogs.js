5ca98be600fb19c20bc07555e3924b3fbaf2cfbb
David <dmolina101@gmail.com>
2024-08-24 13:59:36 -0400

    update

--- models/catalogs.js
+++ models/catalogs.js
@@ -1,5 +1,95 @@
 let db = require('../config/database.js')
 
+const adsTypesEnabled = () => {
+
+    return new Promise(function(resolve, reject) 
+    { 
+
+        let queryString = `SELECT t.ads_type_id,
+                                  t.description AS ads_type_desc 
+                           FROM ads_type t
+                           WHERE status_id = 1
+                           ORDER BY t.description ASC;`
+        db.query(queryString, async function(err, result) 
+        {
+
+            if(err) 
+            {
+    
+                reject({
+                    response: {
+                        message: "Error al tratar de ejecutar la consulta",
+                        status: "error",
+                        statusCode: 0
+                    }
+                })
+    
+            } 
+            else 
+            {
+                
+                resolve({resonse: result})
+    
+            }
+    
+        })
+
+    }).catch(function(error) 
+    {
+
+        console.log(error)
+        return error
+      
+    })
+
+}
+
+const currenciesEnabled = () => {
+
+    return new Promise(function(resolve, reject) 
+    { 
+
+        let queryString = `SELECT c.currency_id,
+                                  c.name AS currency_desc,
+                                  c.abbreviation AS currency_abb,
+                                  c.symbol AS currency_symbol
+                           FROM currency c
+                           WHERE c.status_id = 1
+                           ORDER BY name ASC;`
+        db.query(queryString, async function(err, result) 
+        {
+
+            if(err) 
+            {
+    
+                reject({
+                    response: {
+                        message: "Error al tratar de ejecutar la consulta",
+                        status: "error",
+                        statusCode: 0
+                    }
+                })
+    
+            } 
+            else 
+            {
+                
+                resolve({resonse: result})
+    
+            }
+    
+        })
+
+    }).catch(function(error) 
+    {
+
+        console.log(error)
+        return error
+      
+    })
+
+}
+
 const genders = () => {
 
     return new Promise(function(resolve, reject) 
@@ -41,5 +131,6 @@ const genders = () => {
 }
 
 module.exports = {
+    currenciesEnabled,
     genders
 }
--- routes/catalogs.js
+++ routes/catalogs.js
@@ -2,6 +2,22 @@ var express = require('express')
 var router = express.Router()
 var catalogsModel = require('../models/catalogs.js')
 
+router.get('/ads-types-enabled', async function(req, res, next)
+ {
+
+    let data = await catalogsModel.adsTypesEnabled()
+    res.send(data)
+
+})
+
+router.get('/currencies-enabled', async function(req, res, next)
+ {
+
+    let data = await catalogsModel.currenciesEnabled()
+    res.send(data)
+
+})
+
 router.get('/genders', async function(req, res, next)
  {
 
