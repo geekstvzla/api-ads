a615b897d57e30d87fd7d55dc600597e72235d9d
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2025-01-27 09:09:44 -0400

    update

--- config/database.js
+++ config/database.js
@@ -5,9 +5,10 @@ let settings = {
     host    : process.env.DB_HOST,
     user    : process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    waitForConnections: true
 }
 
-let db = mysql.createConnection(settings)
+let db = mysql.createPool(settings)
   
 module.exports = db
